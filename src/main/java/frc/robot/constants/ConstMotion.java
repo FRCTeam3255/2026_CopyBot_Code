6c41645d77c75a56b33fcb2d6a1e7efdbe767c91
FRCTeam3255-Shared <170778697+FRCTeam3255-Shared-K1-10@users.noreply.github.com>
2026-05-22 21:25:10 -0700

    5-intaking-functional
    i made the intake pivot deploy

--- src/main/java/frc/robot/constants/ConstMotion.java
+++ src/main/java/frc/robot/constants/ConstMotion.java
@@ -4,11 +4,17 @@
 
 package frc.robot.constants;
 
+import static edu.wpi.first.units.Units.Degrees;
+
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
+
+import edu.wpi.first.units.measure.Angle;
 
 /** Add your docs here. */
 public class ConstMotion {
   public static final double STOP = 0;
   public static final TalonFXConfiguration INTAKE_PIVOT_CONFIGURATION = new TalonFXConfiguration();
   public static final TalonFXConfiguration HOOD_PIVOT_CONFIGURATION = new TalonFXConfiguration();
+  public static final Angle INTAKE_PIVOT_DEPLOY = Degrees.of(90);
+  // jayden in the future make a intake retract/
 }
