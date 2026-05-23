8f897f2e6a0a2282dd7444abc11fea8c8cc409ef
FRCTeam3255-Shared <170778697+FRCTeam3255-Shared-K1-10@users.noreply.github.com>
2026-05-23 15:10:47 -0700

    Completed initializing on the shooter hood.
    Co-Authored-By: Tej shah <187054380+Tejshah88@users.noreply.github.com>
    Co-Authored-By: lameesnotlame <258483139+lameesnotlame@users.noreply.github.com>

--- src/main/java/frc/robot/constants/ConstMotion.java
+++ src/main/java/frc/robot/constants/ConstMotion.java
@@ -4,11 +4,16 @@
 
 package frc.robot.constants;
 
+import static edu.wpi.first.units.Units.Degrees;
+
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
+
+import edu.wpi.first.units.measure.Angle;
 
 /** Add your docs here. */
 public class ConstMotion {
   public static final double STOP = 0;
   public static final TalonFXConfiguration INTAKE_PIVOT_CONFIGURATION = new TalonFXConfiguration();
   public static final TalonFXConfiguration HOOD_PIVOT_CONFIGURATION = new TalonFXConfiguration();
+  public static final Angle HOOD_PIVOT_ANGLE = Degrees.of(0);
 }
