6a32b3410eb8dd7c6258b99fe278eda92673e22d
FRCTeam3255-Shared-K1-10 <frcteam3255@gmail.com>
2026-05-23 15:57:19 -0700

    create_rotors_subsystem (#3)
    * I created roters subsytem
    
    James
    Justin
    
    * Fixed names and call functions
    
    * Removed "Rollers" for flywheels
    
    * Added "set" to setter functions
    
    * changed device IDs to reflect comp robot
    
    * create_rotors-subsystem
    
    i added followers
    
    * create rotors
    
    added foollowers ansd can check speed
    
    * create_rotor_subsystem
    
    added configurationhs
    
    * created roters subsystem
    
    fixxed comments
    
    * Create rotor subsystem
    
    Fixed Angela's comment
    
    Co-Authored-By: Justinpham17386769 <244249272+Justinpham17386769@users.noreply.github.com>
    Co-Authored-By: jayden mendoza <244560677+jaymendo670-png@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: FRCTeam3255-Shared <170778697+FRCTeam3255-Shared@users.noreply.github.com>
    Co-authored-by: Justinpham17386769 <244249272+Justinpham17386769@users.noreply.github.com>
    Co-authored-by: jayden mendoza <244560677+jaymendo670-png@users.noreply.github.com>

--- src/main/java/frc/robot/subsystems/Rotors.java
+++ src/main/java/frc/robot/subsystems/Rotors.java
@@ -4,17 +4,108 @@
 
 package frc.robot.subsystems;
 
+import com.ctre.phoenix6.controls.Follower;
+import com.ctre.phoenix6.controls.MotionMagicVelocityVoltage;
+import com.ctre.phoenix6.hardware.TalonFX;
 import edu.wpi.first.epilogue.Logged;
+import edu.wpi.first.units.Units;
+import edu.wpi.first.units.measure.AngularVelocity;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.DeviceIDs.rotorIDs;
+import frc.robot.Robot;
+import frc.robot.constants.ConstRotors;
 
 @Logged
 public class Rotors extends SubsystemBase {
-  /** Creates a new Rotors. */
+
+  final TalonFX serializerRollers = new TalonFX(rotorIDs.SERIALIZER_ROLLERS_CAN);
+  final TalonFX intakeRollersWest = new TalonFX(rotorIDs.INTAKE_ROLLERS_WEST_CAN);
+  final TalonFX intakeRollersEast = new TalonFX(rotorIDs.INTAKE_ROLLERS_EAST_CAN);
+  final TalonFX transferRollersWest = new TalonFX((rotorIDs.TRANSFER_ROLLERS_WEST_CAN));
+  final TalonFX transferRollersEast = new TalonFX((rotorIDs.TRANSFER_ROLLERS_EAST_CAN));
+  final TalonFX flywheelTopWest = new TalonFX((rotorIDs.FLYWHEEL_TOP_WEST_CAN));
+  final TalonFX flywheelTopEast = new TalonFX((rotorIDs.FLYWHEEL_TOP_EAST_CAN));
+  final TalonFX flywheelBottomWest = new TalonFX((rotorIDs.FLYWHEEL_BOTTOM_WEST_CAN));
+  final TalonFX flywheelBottomEast = new TalonFX((rotorIDs.FLYWHEEL_BOTTOM_EAST_CAN));
+  AngularVelocity lastDesiredFlyWheelSpeed = Units.RPM.of(0);
+  AngularVelocity lastDesiredTransferRollersSpeed = Units.RPM.of(0);
+  Follower flywheelEastFollower = new Follower(flywheelTopEast.getDeviceID(), false);
+  Follower flywheelWestFollower = new Follower(flywheelTopEast.getDeviceID(), true);
+  Follower transferRollersEastFollower = new Follower(intakeRollersEast.getDeviceID(), true);
+  Follower intakeRollerEastFollower = new Follower(intakeRollersEast.getDeviceID(), true);
+  private boolean flyWheelAtSpeed = false;
+  // private boolean intakeRollersAtSpeed = false;/
+
   public Rotors() {
+    serializerRollers.getConfigurator().apply(ConstRotors.SERIALIZER_ROLLERS_CONFIGURATION);
+    intakeRollersEast.getConfigurator().apply(ConstRotors.INTAKE_ROLLERS_EAST_CONFIGURATION);
+    intakeRollersWest.getConfigurator().apply(ConstRotors.INTAKE_ROLLERS_WEST_CONFIGURATION);
+    transferRollersEast.getConfigurator().apply(ConstRotors.TRANSFER_ROLLERS_EAST_CONFIGURATION);
+    transferRollersWest.getConfigurator().apply(ConstRotors.TRANSFER_ROLLERS_WEST_CONFIGURATION);
+    flywheelTopEast.getConfigurator().apply(ConstRotors.FLYWHEEL_EAST_CONFIGURATION);
+    flywheelTopWest.getConfigurator().apply(ConstRotors.FLYWHEEL_WEST_CONFIGURATION);
+    flywheelBottomEast.getConfigurator().apply(ConstRotors.FLYWHEEL_EAST_CONFIGURATION);
+    flywheelBottomWest.getConfigurator().apply(ConstRotors.FLYWHEEL_WEST_CONFIGURATION);
   }
 
-  @Override
+  // final MotionMagicVelocityVoltage TransferVelocityRequest = new
+  // MotionMagicVelocityVoltage(0);/
+  final MotionMagicVelocityVoltage flyWheelVelocityRequest = new MotionMagicVelocityVoltage(0);
+
+  public AngularVelocity getFlyWheelSpeeds() {
+    if (Robot.isSimulation()) {
+      return lastDesiredFlyWheelSpeed;
+    }
+    return flywheelTopEast.getVelocity().getValue();
+  }
+
+  public AngularVelocity getSerializerRollersVelocity() {
+    return serializerRollers.getVelocity().getValue();
+  }
+
+  public AngularVelocity getIntakeRollersVelocity() {
+    return intakeRollersEast.getVelocity().getValue();
+  }
+
+  public AngularVelocity getTransferRollersVelocity() {
+    return transferRollersEast.getVelocity().getValue();
+  }
+
+  public void setSerializerRollersSpeed(double speed) {
+    serializerRollers.set(speed);
+  }
+
+  public void setIntakeRollersSpeeds(double speed) {
+    intakeRollersEast.set(speed);
+    intakeRollersWest.setControl(intakeRollerEastFollower);
+  }
+
+  public void setTransferRollersSpeeds(double speed) {
+    transferRollersEast.set(speed);
+    transferRollersWest.setControl(transferRollersEastFollower);
+  }
+
+  public void setFlywheelSpeeds(AngularVelocity speed) {
+    flywheelTopEast.setControl(flyWheelVelocityRequest.withVelocity(speed));
+    flywheelTopWest.setControl(flywheelWestFollower);
+    flywheelBottomWest.setControl(flywheelWestFollower);
+    flywheelBottomEast.setControl(flywheelEastFollower);
+    lastDesiredFlyWheelSpeed = speed;
+  }
+
+  public boolean isFlyWheelAtSpeed(AngularVelocity tolerance) {
+    AngularVelocity lowerLim = lastDesiredFlyWheelSpeed.minus(tolerance);
+    AngularVelocity upperLim = lastDesiredFlyWheelSpeed.plus(tolerance);
+
+    AngularVelocity flyWheelSpeed = getFlyWheelSpeeds();
+
+    flyWheelAtSpeed = flyWheelSpeed.gte(lowerLim)
+        && flyWheelSpeed.lte(upperLim);
+    return flyWheelAtSpeed;
+
+  }
+
   public void periodic() {
-    // This method will be called once per scheduler run
+
   }
 }
