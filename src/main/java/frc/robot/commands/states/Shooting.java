826744c6020faf422b326709d8da68682f8f0eee
jayden mendoza <jaymendo670@euhsd.org>
2026-05-23 19:52:04 -0700

    6 shooting functional (#9)
    * Completed initializing on the shooter hood.
    
    Co-Authored-By: Tej shah <187054380+Tejshah88@users.noreply.github.com>
    Co-Authored-By: lameesnotlame <258483139+lameesnotlame@users.noreply.github.com>
    
    * Added shooting functionality
    
    Co-Authored-By: Tej shah <187054380+Tejshah88@users.noreply.github.com>
    Co-Authored-By: lameesnotlame <258483139+lameesnotlame@users.noreply.github.com>
    
    * Update Shooting.java
    
    Co-Authored-By: Tej shah <187054380+Tejshah88@users.noreply.github.com>
    Co-Authored-By: lameesnotlame <258483139+lameesnotlame@users.noreply.github.com>
    
    * Changed line 18 in ConstMotion to 14 degrees
    
    Co-Authored-By: lameesnotlame <258483139+lameesnotlame@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: FRCTeam3255-Shared <170778697+FRCTeam3255-Shared-K1-10@users.noreply.github.com>
    Co-authored-by: Tej shah <187054380+Tejshah88@users.noreply.github.com>
    Co-authored-by: lameesnotlame <258483139+lameesnotlame@users.noreply.github.com>

---
+++ src/main/java/frc/robot/commands/states/Shooting.java
@@ -0,0 +1,54 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands.states;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.RobotContainer;
+import frc.robot.constants.ConstMotion;
+import frc.robot.constants.ConstRotors;
+
+public class Shooting extends Command {
+  /** Creates a new Shooting. */
+  public Shooting() {
+
+    // Use addRequirements() here to declare subsystem dependencies.
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    RobotContainer.motionInstance.setHoodPivotAngle(ConstMotion.HOOD_PIVOT_ANGLE);
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+
+    RobotContainer.rotorsInstance.setFlywheelSpeeds(ConstRotors.FLYWHEEL_SHOOTING_SPEED);
+    if (RobotContainer.rotorsInstance.isFlyWheelAtSpeed(ConstRotors.FLYWHEEL_SHOOTING_SPEED)) {
+      RobotContainer.rotorsInstance.setTransferRollersSpeeds(ConstRotors.INTAKE_TRANSFER_SPEED);
+      RobotContainer.rotorsInstance.setSerializerRollersSpeed(ConstRotors.SERIALIZER_SHOOTING_SPEED);
+
+    } else {
+      RobotContainer.rotorsInstance.setTransferRollersSpeeds(ConstRotors.STOP_ALL);
+    }
+
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    RobotContainer.rotorsInstance.setFlywheelSpeeds(ConstRotors.STOP_ALL);
+    RobotContainer.rotorsInstance.setTransferRollersSpeeds(ConstRotors.STOP_ALL);
+    RobotContainer.rotorsInstance.setSerializerRollersSpeed(ConstRotors.STOP_ALL);
+
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
