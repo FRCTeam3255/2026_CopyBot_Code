2b473c04eef2b08b5b68686c1a59985fbae48411
Yutong Zhu <3681247216@qq.com>
2026-05-30 13:17:38 -0700

    State machine update (#32)
    * qf
    
    * rename

--- src/main/java/frc/robot/commands/states/PrepTower.java
+++ src/main/java/frc/robot/commands/states/PrepTower.java
@@ -6,23 +6,26 @@ package frc.robot.commands.states;
 
 import edu.wpi.first.wpilibj2.command.Command;
 
-public class PrepOutpost extends Command {
+public class PrepTower extends Command {
   /** Creates a new PrepOutpost. */
-  public PrepOutpost() {
+  public PrepTower() {
     // Use addRequirements() here to declare subsystem dependencies.
   }
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {}
+  public void initialize() {
+  }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {}
+  public void execute() {
+  }
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) {
+  }
 
   // Returns true when the command should end.
   @Override
