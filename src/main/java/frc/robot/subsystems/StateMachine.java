3e98a3787374198cdc9c42a33a30a5d99421b0fd
FRCTeam3255-Shared <170778697+FRCTeam3255-Shared@users.noreply.github.com>
2026-05-30 19:57:31 -0700

    statev machine button mapping
    Co-Authored-By: lameesnotlame <258483139+lameesnotlame@users.noreply.github.com>

--- src/main/java/frc/robot/subsystems/StateMachine.java
+++ src/main/java/frc/robot/subsystems/StateMachine.java
@@ -8,7 +8,18 @@ import edu.wpi.first.epilogue.Logged;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.commands.states.EjectingHopper;
+import frc.robot.commands.states.Intaking;
 import frc.robot.commands.states.None;
+import frc.robot.commands.states.PrepAnywhere;
+import frc.robot.commands.states.PrepCorner;
+import frc.robot.commands.states.PrepHub;
+import frc.robot.commands.states.PrepNeautralToAlliance;
+import frc.robot.commands.states.PrepOpponentToAlliance;
+import frc.robot.commands.states.PrepTower;
+import frc.robot.commands.states.PrepTrench;
+import frc.robot.commands.states.ReversingShooter;
+import frc.robot.commands.states.Shooting;
 
 @Logged
 public class StateMachine extends SubsystemBase {
@@ -36,6 +47,7 @@ public class StateMachine extends SubsystemBase {
           case NONE:
           case INTAKING:
           case REVERSING_SHOOTER:
+          case SHOOTING:
           case EJECTING_HOPPER:
             return new None();
         }
@@ -43,77 +55,162 @@ public class StateMachine extends SubsystemBase {
       case INTAKING:
         switch (currentRobotState) {
           case NONE:
-            return new None();
+          case RETRACT_INTAKE:
+          case PREP_ANYWHERE:
+          case PREP_CORNER:
+          case PREP_HUB:
+          case PREP_NEUTRAL_TO_ALLIANCE:
+          case PREP_OPPONENT_TO_ALLIANCE:
+          case PREP_TOWER:
+          case PREP_TRENCH:
+            return new Intaking();
         }
         break;
       case SHOOTING:
         switch (currentRobotState) {
-          case NONE:
-            return new None();
+          case RETRACT_INTAKE:
+          case PREP_ANYWHERE:
+          case PREP_CORNER:
+          case PREP_HUB:
+          case PREP_NEUTRAL_TO_ALLIANCE:
+          case PREP_OPPONENT_TO_ALLIANCE:
+          case PREP_TOWER:
+          case PREP_TRENCH:
+            return new Shooting();
         }
         break;
       case REVERSING_SHOOTER:
         switch (currentRobotState) {
           case NONE:
-            return new None();
+          case RETRACT_INTAKE:
+          case PREP_ANYWHERE:
+          case PREP_CORNER:
+          case PREP_HUB:
+          case PREP_NEUTRAL_TO_ALLIANCE:
+          case PREP_OPPONENT_TO_ALLIANCE:
+          case PREP_TOWER:
+          case PREP_TRENCH:
+            return new ReversingShooter();
         }
         break;
       case PREP_ANYWHERE:
         switch (currentRobotState) {
-          case EJECTING_HOPPER:
+          case NONE:
+          case RETRACT_INTAKE:
+          case PREP_ANYWHERE:
+          case PREP_CORNER:
+          case PREP_HUB:
+          case PREP_NEUTRAL_TO_ALLIANCE:
+          case PREP_OPPONENT_TO_ALLIANCE:
+          case PREP_TOWER:
+          case PREP_TRENCH:
           case REVERSING_SHOOTER:
-          case SHOOTING:
-            return new None();
+            return new PrepAnywhere();
         }
         break;
       case PREP_HUB:
         switch (currentRobotState) {
-          case EJECTING_HOPPER:
+          case NONE:
+          case RETRACT_INTAKE:
+          case PREP_ANYWHERE:
+          case PREP_CORNER:
+          case PREP_HUB:
+          case PREP_NEUTRAL_TO_ALLIANCE:
+          case PREP_OPPONENT_TO_ALLIANCE:
+          case PREP_TOWER:
+          case PREP_TRENCH:
           case REVERSING_SHOOTER:
-          case SHOOTING:
-            return new None();
+            return new PrepHub();
         }
         break;
       case PREP_CORNER:
         switch (currentRobotState) {
-          case EJECTING_HOPPER:
+          case NONE:
+          case RETRACT_INTAKE:
+          case PREP_ANYWHERE:
+          case PREP_CORNER:
+          case PREP_HUB:
+          case PREP_NEUTRAL_TO_ALLIANCE:
+          case PREP_OPPONENT_TO_ALLIANCE:
+          case PREP_TOWER:
+          case PREP_TRENCH:
           case REVERSING_SHOOTER:
-          case SHOOTING:
-            return new None();
+            return new PrepCorner();
         }
         break;
       case PREP_NEUTRAL_TO_ALLIANCE:
         switch (currentRobotState) {
-          case EJECTING_HOPPER:
+          case NONE:
+          case RETRACT_INTAKE:
+          case PREP_ANYWHERE:
+          case PREP_CORNER:
+          case PREP_HUB:
+          case PREP_NEUTRAL_TO_ALLIANCE:
+          case PREP_OPPONENT_TO_ALLIANCE:
+          case PREP_TOWER:
+          case PREP_TRENCH:
           case REVERSING_SHOOTER:
-          case SHOOTING:
-            return new None();
+            return new PrepNeautralToAlliance();
         }
         break;
       case PREP_OPPONENT_TO_ALLIANCE:
         switch (currentRobotState) {
-          case EJECTING_HOPPER:
+          case NONE:
+          case RETRACT_INTAKE:
+          case PREP_ANYWHERE:
+          case PREP_CORNER:
+          case PREP_HUB:
+          case PREP_NEUTRAL_TO_ALLIANCE:
+          case PREP_OPPONENT_TO_ALLIANCE:
+          case PREP_TOWER:
+          case PREP_TRENCH:
           case REVERSING_SHOOTER:
-          case SHOOTING:
-            return new None();
+            return new PrepOpponentToAlliance();
         }
         break;
       case PREP_TOWER:
         switch (currentRobotState) {
-          case EJECTING_HOPPER:
+          case NONE:
+          case RETRACT_INTAKE:
+          case PREP_ANYWHERE:
+          case PREP_CORNER:
+          case PREP_HUB:
+          case PREP_NEUTRAL_TO_ALLIANCE:
+          case PREP_OPPONENT_TO_ALLIANCE:
+          case PREP_TOWER:
+          case PREP_TRENCH:
           case REVERSING_SHOOTER:
-          case SHOOTING:
-            return new None();
+            return new PrepTower();
         }
         break;
       case PREP_TRENCH:
         switch (currentRobotState) {
-          case EJECTING_HOPPER:
+          case NONE:
+          case RETRACT_INTAKE:
+          case PREP_ANYWHERE:
+          case PREP_CORNER:
+          case PREP_HUB:
+          case PREP_NEUTRAL_TO_ALLIANCE:
+          case PREP_OPPONENT_TO_ALLIANCE:
+          case PREP_TOWER:
+          case PREP_TRENCH:
           case REVERSING_SHOOTER:
-          case SHOOTING:
-            return new None();
+            return new PrepTrench();
         }
         break;
+      case EJECTING_HOPPER:
+        switch (currentRobotState) {
+          case NONE:
+          case RETRACT_INTAKE:
+          case PREP_ANYWHERE:
+          case PREP_CORNER:
+          case PREP_HUB:
+          case PREP_NEUTRAL_TO_ALLIANCE:
+          case PREP_OPPONENT_TO_ALLIANCE:
+          case PREP_TOWER:
+          case PREP_TRENCH:
+            return new EjectingHopper();
+        }
 
     }
     return Commands
