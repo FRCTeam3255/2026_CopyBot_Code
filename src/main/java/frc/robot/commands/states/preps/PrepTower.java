878a69d79a9bfd4268885c8e2e43fa994e05cee9
FRCTeam3255-Shared <frcteam3255@gmail.com>
2026-05-30 17:29:45 -0700

    hood angles will change depending on each prep
    james
    
    Co-Authored-By: Justinpham17386769 <244249272+Justinpham17386769@users.noreply.github.com>

---
+++ src/main/java/frc/robot/commands/states/preps/PrepTower.java
@@ -0,0 +1,19 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands.states.preps;
+
+import frc.robot.constants.ConstMotion;
+import frc.robot.constants.ConstRotors;
+import frc.robot.subsystems.StateMachine.RobotState;
+
+public class PrepTower extends BasePrep {
+  /** Creates a new PrepOutpost. */
+  public PrepTower() {
+
+    super(ConstRotors.FLYWHEEL_TOWER_SPEED, ConstMotion.HOOD_TOWER_ANGLE, RobotState.PREP_TOWER);
+
+    // Use addRequirements() here to declare subsystem dependencies.
+  }
+}
