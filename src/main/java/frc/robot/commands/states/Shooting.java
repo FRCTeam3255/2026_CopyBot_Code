3f9c19f555e716bbf43d108185ca651d19f56e14
FRCTeam3255-Shared <170778697+FRCTeam3255-Shared@users.noreply.github.com>
2026-06-12 20:06:46 -0700

    Fix redundant and confusing constants

--- src/main/java/frc/robot/commands/states/Shooting.java
+++ src/main/java/frc/robot/commands/states/Shooting.java
@@ -28,7 +28,7 @@ public class Shooting extends Command {
   @Override
   public void execute() {
     RobotContainer.rotorsInstance.setTransferRollersSpeeds(ConstRotors.TRANSFER_ROLLERS_SPEED);
-    RobotContainer.rotorsInstance.setSerializerRollersPercentOutput(ConstRotors.SERIALIZER_ROLLERS_SPEED);
+    RobotContainer.rotorsInstance.setSerializerRollersPercentOutput(ConstRotors.SERIALIZER_SHOOTER_ROLLERS_SPEED);
 
   }
 
